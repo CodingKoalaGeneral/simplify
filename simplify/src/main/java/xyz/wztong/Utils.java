d0c643d98dbd7cc2fc4154f1016bc2227b56dab2
WZTong <wztong@vip.qq.com>
2023-12-31 23:54:43 +0800

    Refactor function name

--- simplify/src/main/java/xyz/wztong/Utils.java
+++ simplify/src/main/java/xyz/wztong/Utils.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Utils {
 
-    public static void classManagerFactoryDisassemble(File input, File output) {
+    public static void disassembleDex(File input, File output) {
         org.jf.baksmali.Main.main(new String[]{"disassemble", "--use-locals", "--sequential-labels", "--code-offsets", input.getAbsolutePath(), "--output", output.toString()});
     }
 }
