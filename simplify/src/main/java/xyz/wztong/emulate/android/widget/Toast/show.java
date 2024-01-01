a26f1b692ec094ca235134cde7ba8edf7b7baefa
WZTong <wztong@vip.qq.com>
2024-01-01 21:20:48 +0800

    Toast show implementation

---
+++ simplify/src/main/java/xyz/wztong/emulate/android/widget/Toast/show.java
@@ -0,0 +1,21 @@
+package xyz.wztong.emulate.android.widget.Toast;
+
+import org.cf.smalivm.VirtualMachine;
+import org.cf.smalivm.context.MethodState;
+import org.cf.smalivm.emulate.MethodStateMethod;
+import org.cf.smalivm.opcode.Op;
+import xyz.wztong.Utils;
+import xyz.wztong.type.android.widget.Toast;
+
+public class show extends MethodStateMethod {
+    @Override
+    protected void execute(VirtualMachine vm, Op op, MethodState mState) {
+        var toastHeap = mState.peekParameter(0);
+        CharSequence toastText = "*UNKNOWN*";
+        Toast toast;
+        if(toastHeap.isKnown() && (toast = (Toast) toastHeap.getValue())!=null){
+            toastText = String.valueOf(toast.text);
+        }
+        Utils.printExecutingFunction(toastText);
+    }
+}
