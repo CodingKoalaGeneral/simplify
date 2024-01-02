1f7c7054f7905654f0d9d10de7df9819227e37cf
WZTong <wztong@vip.qq.com>
2024-01-02 16:42:59 +0800

    Make all self-defined function side effect level to "STRONG" to avoid being deleted

--- simplify/src/main/java/xyz/wztong/emulate/EmulateMethodStateMethod.java
+++ simplify/src/main/java/xyz/wztong/emulate/EmulateMethodStateMethod.java
@@ -1,7 +1,12 @@
 package xyz.wztong.emulate;
 
+import org.cf.smalivm.SideEffect;
 import org.cf.smalivm.emulate.MethodStateMethod;
 import org.cf.smalivm.emulate.UnknownValuesMethod;
 
 public abstract class EmulateMethodStateMethod extends MethodStateMethod implements UnknownValuesMethod {
+    @Override
+    public SideEffect.Level getSideEffectLevel() {
+        return SideEffect.Level.STRONG;
+    }
 }
