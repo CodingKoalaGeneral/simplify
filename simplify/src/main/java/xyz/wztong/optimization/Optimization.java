e1eaa2333537468465d0b7cc5c8c377165fa081f
WZTong <wztong@vip.qq.com>
2024-01-02 17:23:45 +0800

    Add safety belts

--- simplify/src/main/java/xyz/wztong/optimization/Optimization.java
+++ simplify/src/main/java/xyz/wztong/optimization/Optimization.java
@@ -2,6 +2,7 @@ package xyz.wztong.optimization;
 
 import org.cf.simplify.ExecutionGraphManipulator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
@@ -12,7 +13,12 @@ public interface Optimization {
     int perform(ExecutionGraphManipulator manipulator);
 
     default List<Integer> getValidAddresses(ExecutionGraphManipulator manipulator, Predicate<? super Integer> filter) {
-        return IntStream.of(manipulator.getAddresses()).boxed().filter(filter).sorted(Collections.reverseOrder()).toList();
+        var addresses = manipulator.getAddresses();
+        // Maybe failed to execute
+        if (addresses.length == 0) {
+            return new ArrayList<>();
+        }
+        return IntStream.of(addresses).boxed().filter(filter).sorted(Collections.reverseOrder()).toList();
     }
 
 }
