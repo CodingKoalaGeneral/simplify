4545937467f730361ea3a54b2aa7131a40f9df3d
WZTong <wztong@vip.qq.com>
2024-01-02 23:17:34 +0800

    Finished setup before starting cfg de-obf

--- simplify/src/main/java/xyz/wztong/Main.java
+++ simplify/src/main/java/xyz/wztong/Main.java
@@ -1,45 +1,51 @@
 package xyz.wztong;
 
 import org.cf.smalivm.VirtualMachineFactory;
+import org.cf.smalivm.context.ExecutionGraph;
 import org.cf.smalivm.exception.VirtualMachineException;
 import org.jf.dexlib2.writer.io.FileDataStore;
 import xyz.wztong.optimization.Optimizer;
-import xyz.wztong.type.android.app.Activity;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SuppressWarnings("unused")
 public class Main {
 
     public static void main(String[] args) throws IOException, VirtualMachineException {
-        testObfuscatedApp(args);
+        testSimplified(args);
     }
 
-    private static final int DEFAULT_MAX_ADDRESS_VISITS = Integer.MAX_VALUE;
-    private static final int DEFAULT_MAX_CALL_DEPTH = 1;
-    private static final int DEFAULT_MAX_EXECUTION_TIME = 5 * 60;
-    private static final int DEFAULT_MAX_METHOD_VISITS = 1_000_000;
+    private static void testSimplified(String[] args) throws IOException, VirtualMachineException {
+        var disassembleAddress = "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified";
+        var vm = new VirtualMachineFactory().build(disassembleAddress);
+        var graph = vm.execute("Lcom/example/check/MainActivity;->onCreate(Landroid/os/Bundle;)V");
+        System.out.println(IntStream.of(graph.getAddresses()).mapToObj(addr -> Integer.toHexString(addr) + " => " + graph.wasAddressReached(addr) + " => " + Objects.requireNonNull(graph.getOp(addr))).collect(Collectors.joining("\n")));
+        Optimizer.optimize(graph);
+        System.out.println();
+    }
 
     public static void testObfuscatedApp(String[] args) throws IOException, VirtualMachineException {
-//        Utils.disassembleDex(new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\classes.dex"), new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\disassemble"));
-//        Utils.disassembleDex(new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\classes.dex"), new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\disassemble"));
-        var disassembleAddress = "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\disassemble";
+//        Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\classes.renamed.dex", "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\disassemble");
+//        Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\classes.dex"  ,"C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\disassemble");
+        var disassembleAddress = "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\disassemble";
         var vm = new VirtualMachineFactory().build(disassembleAddress);
-        var ctx = vm.spawnRootContext("Lcom/example/check/MainActivity;->onCreate(Landroid/os/Bundle;)V");
-        var mState = ctx.getMethodState();
-        Utils.setParameter(mState, new Activity(), "Lcom/example/check/MainActivity;", "Bundle", "Landroid/os/Bundle;");
-        var graph = vm.execute(ctx);
-        Optimizer.optimize(graph);
-    }
 
-    public static void testLocalSmali(String[] args) throws IOException, VirtualMachineException {
-        var vm = new VirtualMachineFactory().build("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong");
-        Optimizer.optimize(vm.execute("LTest;->helloWorld()V"));
-        Optimizer.optimize(vm.execute("LTest;->invokeIntegerMethods()I"));
-        Optimizer.optimize(vm.execute("LTest;->getIntegerMethod()I"));
-        vm.getClassManager().getDexBuilder().writeTo(new FileDataStore(new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\out\\out.dex")));
-        Utils.disassembleDex(new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\out\\" + "out.dex"), new File("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\out\\"));
+        int optimize = -1;
+        var method = vm.getClassManager().getMethod("Lcom/example/check/MainActivity;->onCreate(Landroid/os/Bundle;)V");
+        ExecutionGraph graph;
+        int optimizeCount = 0;
+        while (optimize != 0) {
+            graph = vm.execute(method);
+            optimizeCount++;
+            Utils.print("OptimizeCount: " + optimizeCount);
+            optimize = Optimizer.optimize(graph);
+        }
+        Utils.writeDex(vm, "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified.dex");
+        Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified.dex", "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified\\");
     }
 
 }
