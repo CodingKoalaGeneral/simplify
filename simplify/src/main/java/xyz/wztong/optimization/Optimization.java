0c6318c666554559ca5f91d3769957f0730c4169
WZTong <wztong@vip.qq.com>
2024-01-02 22:55:41 +0800

    Add utility method processValidAddresses

--- simplify/src/main/java/xyz/wztong/optimization/Optimization.java
+++ simplify/src/main/java/xyz/wztong/optimization/Optimization.java
@@ -5,6 +5,8 @@ import org.cf.simplify.ExecutionGraphManipulator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
@@ -12,13 +14,22 @@ public interface Optimization {
 
     int perform(ExecutionGraphManipulator manipulator);
 
-    default List<Integer> getValidAddresses(ExecutionGraphManipulator manipulator, Predicate<? super Integer> filter) {
+    default List<Integer> getValidAddresses(ExecutionGraphManipulator manipulator, Predicate<Integer> filter) {
         var addresses = manipulator.getAddresses();
         // Maybe failed to execute
         if (addresses.length == 0) {
             return new ArrayList<>();
         }
         return IntStream.of(addresses).boxed().filter(filter).sorted(Collections.reverseOrder()).toList();
+    }
+
+    default <T> void processValidAddresses(ExecutionGraphManipulator manipulator, Predicate<Integer> filter, Function<Integer, T> mapper, Consumer<T> consumer) {
+        var addresses = manipulator.getAddresses();
+        // Maybe failed to execute
+        if (addresses.length == 0) {
+            return;
+        }
+        IntStream.of(addresses).boxed().filter(filter).map(mapper).sorted(Collections.reverseOrder()).forEach(consumer);
     }
 
 }
