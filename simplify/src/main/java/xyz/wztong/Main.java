ff6e292b5c62c2eae763869b38a31d107512c784
WZTong <wztong@vip.qq.com>
2024-01-03 16:48:16 +0800

    Handy to simplify example app

--- simplify/src/main/java/xyz/wztong/Main.java
+++ simplify/src/main/java/xyz/wztong/Main.java
@@ -1,28 +1,28 @@
 package xyz.wztong;
 
 import org.cf.smalivm.VirtualMachineFactory;
-import org.cf.smalivm.context.ExecutionGraph;
 import org.cf.smalivm.exception.VirtualMachineException;
 import xyz.wztong.optimization.Optimizer;
+import xyz.wztong.optimization.impl.ConstantSwitchSeekBack;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Set;
 
 @SuppressWarnings("unused")
 public class Main {
 
     public static void main(String[] args) throws IOException, VirtualMachineException {
-        testSimplified(args);
+        testObfuscatedApp(args);
     }
 
     private static void testSimplified(String[] args) throws IOException, VirtualMachineException {
         var disassembleAddress = "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified";
         var vm = new VirtualMachineFactory().build(disassembleAddress);
         var graph = vm.execute("Lcom/example/check/MainActivity;->onCreate(Landroid/os/Bundle;)V");
-        System.out.println(IntStream.of(graph.getAddresses()).mapToObj(addr -> Integer.toHexString(addr) + " => " + graph.wasAddressReached(addr) + " => " + Objects.requireNonNull(graph.getOp(addr))).collect(Collectors.joining("\n")));
-        Optimizer.optimize(graph);
+        Optimizer.optimize(Set.of(new ConstantSwitchSeekBack()), graph);
+//        Optimizer.optimize(graph);
+        Utils.writeDex(vm, "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\test.dex");
+        Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\test.dex", "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\test\\");
         System.out.println();
     }
 
@@ -31,17 +31,11 @@ public class Main {
 //        Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\classes.dex"  ,"C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Original\\disassemble");
         var disassembleAddress = "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\disassemble";
         var vm = new VirtualMachineFactory().build(disassembleAddress);
-
-        int optimize = -1;
         var method = vm.getClassManager().getMethod("Lcom/example/check/MainActivity;->onCreate(Landroid/os/Bundle;)V");
-        ExecutionGraph graph;
-        int optimizeCount = 0;
-        while (optimize != 0) {
-            graph = vm.execute(method);
-            optimizeCount++;
-            Utils.print("OptimizeCount: " + optimizeCount);
-            optimize = Optimizer.optimize(graph);
+        while (true) {
+            if (Optimizer.optimize(vm.execute(method)) == 0) break;
         }
+//        Optimizer.optimize(Set.of(new ConstantSwitchSeekBack()), vm.execute(method));
         Utils.writeDex(vm, "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified.dex");
         Utils.disassembleDex("C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified.dex", "C:\\Users\\WZTong\\Downloads\\IdeaProjectOffline\\DalvikDeObf\\simplify\\src\\main\\resources\\wztong\\ExampleToast\\Obfuscated\\simplified\\");
     }
