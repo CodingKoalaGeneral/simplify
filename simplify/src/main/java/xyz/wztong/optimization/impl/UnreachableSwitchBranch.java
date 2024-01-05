2e090618a65466ae99d7361f7be330ececac55b3
WZTong <wztong@vip.qq.com>
2024-01-05 21:56:20 +0800

    Make it ReExecute to accurate values

--- simplify/src/main/java/xyz/wztong/optimization/impl/UnreachableSwitchBranch.java
+++ simplify/src/main/java/xyz/wztong/optimization/impl/UnreachableSwitchBranch.java
@@ -10,7 +10,7 @@ import xyz.wztong.optimization.Optimization;
 
 import java.util.ArrayList;
 
-public class UnreachableSwitchBranch implements Optimization.ReOptimize {
+public class UnreachableSwitchBranch implements Optimization.ReExecute {
     @Override
     public int perform(ExecutionGraphManipulator manipulator) {
         var switchOpAddresses = getValidAddresses(manipulator, address -> {
