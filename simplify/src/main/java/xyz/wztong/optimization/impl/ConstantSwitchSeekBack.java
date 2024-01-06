7a53a8eda97fc3d0b10db606dba42f64490debb1
WZTong <wztong@vip.qq.com>
2024-01-06 14:41:25 +0800

    Make parent seek back smarter

--- simplify/src/main/java/xyz/wztong/optimization/impl/ConstantSwitchSeekBack.java
+++ simplify/src/main/java/xyz/wztong/optimization/impl/ConstantSwitchSeekBack.java
@@ -1,6 +1,7 @@
 package xyz.wztong.optimization.impl;
 
 import gnu.trove.map.TIntIntMap;
+import gnu.trove.set.hash.TIntHashSet;
 import org.cf.simplify.ExecutionGraphManipulator;
 import org.cf.smalivm.context.ExecutionNode;
 import org.cf.smalivm.opcode.*;
@@ -8,9 +9,7 @@ import xyz.wztong.Utils;
 import xyz.wztong.optimization.Optimization;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ConstantSwitchSeekBack implements Optimization.ReExecute{
@@ -54,7 +53,6 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
             var targetKeyToOffset = getTargetKeyToOffset(switchPayloadOp);
             var switchRegister = getRegister(switchOp);
             var nodePile = manipulator.getNodePile(address);
-            // Node(switchValue -> offset)
             var validNodesWithSwitchAddress = new ArrayList<Map.Entry<ExecutionNode, Integer>>();
             for (var node : nodePile) {
                 var switchHeap = node.getContext().getMethodState().peekRegister(switchRegister);
@@ -79,12 +77,12 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
                 var node = validNodeWithSwitchAddress.getKey();
                 var targetAddress = validNodeWithSwitchAddress.getValue();
                 var aNode = new AtomicReference<>(node);
-                var constRegister = new AtomicInteger(switchRegister);
-                var affectedRegisters = new ArrayList<Integer>();
+                var constRegisters = new TIntHashSet();
+                constRegisters.add(switchRegister);
                 ExecutionNode parentNode = null;
                 NextSeekBack:
                 for (int i = 0; i < seekBackLimit; i++) {
-                    var updateResult = updateParent(aNode, constRegister, affectedRegisters);
+                    var updateResult = updateParent(aNode, constRegisters);
                     switch (updateResult) {
                         case NOT_FOUND -> {
                             continue NextNode;
@@ -93,8 +91,10 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
                             // continue NextSeekBack;
                         }
                         case SIDE_EFFECT -> {
-                            throw new RuntimeException("AAA");
+                            System.out.println(updateResult);
                         }
+                        case UNKNOWN ->
+                                throw new IllegalStateException("What happened? Unknown structure for parent finding");
                         case CONST_SOURCE -> {
                             parentNode = aNode.get();
                             break NextSeekBack;
@@ -117,20 +117,18 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
     }
 
     private enum ConstantParentStatus {
-        CONST_SOURCE, SEEK_BACK, SIDE_EFFECT, NOT_FOUND
+        CONST_SOURCE, SEEK_BACK, SIDE_EFFECT, NOT_FOUND, UNKNOWN
     }
 
-    /**
-     * Return:
-     * "ExecutionNode": If const parent find
-     * "nullptr"      : If no const parent find
-     * "InputNode"    : If this is the const source
-     */
-    private static ConstantParentStatus updateParent(AtomicReference<ExecutionNode> node, AtomicInteger constRegister, List<Integer> affectedRegisters) {
+    private static ConstantParentStatus updateParent(AtomicReference<ExecutionNode> node, TIntHashSet constRegisters) {
         var currentNode = node.get();
         var parentNode = currentNode.getParent();
         var currentOp = currentNode.getOp();
         if (parentNode == null) {
+            return ConstantParentStatus.NOT_FOUND;
+        }
+        if (constRegisters.isEmpty()) {
+            // TODO: Determine NOT_FOUND or CONST_SROUCE
             return ConstantParentStatus.NOT_FOUND;
         }
         var parentOp = parentNode.getOp();
@@ -138,41 +136,65 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
             // The value source from a method/instruction has side effect, cannot optimize
             return ConstantParentStatus.NOT_FOUND;
         }
-        if (parentOp instanceof ConstOp constOp) {
+        if (parentOp instanceof GotoOp) {
+            // Goto has no side effect, all resgisters keeps the same
+            node.set(parentNode);
+            return ConstantParentStatus.SEEK_BACK;
+        } else if (parentOp instanceof ConstOp constOp) {
             // const(?) current <= constVal      ----  parent op
             // unknown-instruction               ----  current op
-            if (constOp.getDestRegister() == constRegister.get()) {
+            var destRegister = constOp.getDestRegister();
+            if (constRegisters.contains(destRegister)) {
                 // Prevent searching deeper
                 node.set(parentNode);
-                return ConstantParentStatus.CONST_SOURCE;
-            } else {
-                // TODO: Side effect may occur when seek back deeper, but it's safe here?
-                return ConstantParentStatus.NOT_FOUND;
-            }
-        } else if (parentOp instanceof MoveOp moveOp) {
-            if (moveOp.getToRegister() != constRegister.get()) {
-                // Side effect may occur
-                return ConstantParentStatus.NOT_FOUND;
-                // No need to check if source is constant
-            }
-            var moveType = getMoveTypeString(moveOp);
-            if (moveType.equals("REGISTER")) {
-                // move(-object) current <= source
-                // Set the tracking const regster to the source
-                constRegister.set(moveOp.getTargetRegister());
-                node.set(parentNode);
-                return ConstantParentStatus.SEEK_BACK;
-            } else if (moveType.equals("RESULT")) {
-                // move-result(-object) will handle in InvokeOp, check and pass to its parent
-                var possibleInvokeOp = parentNode.getParent().getOp();
-                if (possibleInvokeOp instanceof InvokeOp) {
-                    node.set(parentNode);
+                constRegisters.remove(destRegister);
+                if (constRegisters.isEmpty()) {
+                    // This is the last constant source, cannot seek further
+                    return ConstantParentStatus.CONST_SOURCE;
+                } else {
                     return ConstantParentStatus.SEEK_BACK;
                 }
             } else {
-                // MoveType(EXCEPTION) will have side effects
                 return ConstantParentStatus.NOT_FOUND;
             }
+        } else if (parentOp instanceof MoveOp moveOp) {
+            int toRegister = moveOp.getToRegister();
+            if (constRegisters.contains(toRegister)) {
+                var moveType = getMoveTypeString(moveOp);
+                if (moveType.equals("REGISTER")) {
+                    // move(-object) current <= source
+                    // No need to check if source is constant
+                    // Set the tracking const regster to the source
+                    constRegisters.add(moveOp.getTargetRegister());
+                    node.set(parentNode);
+                    return ConstantParentStatus.SEEK_BACK;
+                } else if (moveType.equals("RESULT")) {
+                    // move-result(-object) will handle in InvokeOp, check and pass to its parent
+                    var possibleInvokeOp = parentNode.getParent().getOp();
+                    if (possibleInvokeOp instanceof InvokeOp) {
+                        node.set(parentNode);
+                        return ConstantParentStatus.SEEK_BACK;
+                    } else {
+                        return ConstantParentStatus.UNKNOWN;
+                    }
+                } else {
+                    // MoveType(EXCEPTION) will have side effects
+                    return ConstantParentStatus.NOT_FOUND;
+                }
+            } else {
+                var currentRegister = currentNode.getContext().getMethodState().peekRegister(toRegister);
+                if (currentRegister == null) {
+                    return ConstantParentStatus.NOT_FOUND;
+                }
+                // Record, then skip const object movement to search deeper
+                if (currentRegister.isImmutable() && currentRegister.isKnown()) {
+                    constRegisters.add(moveOp.getTargetRegister());
+                    node.set(parentNode);
+                    return ConstantParentStatus.SIDE_EFFECT;
+                }
+            }
+            // Side effect may occur
+            return ConstantParentStatus.NOT_FOUND;
         } else if (parentOp instanceof InvokeOp invokeOp) {
             var parameterRegisters = invokeOp.getParameterRegisters();
             if (parameterRegisters.length > 1) {
@@ -192,18 +214,19 @@ public class ConstantSwitchSeekBack implements Optimization.ReExecute{
                 if (!getMoveTypeString(moveOp).equals("RESULT")) {
                     return ConstantParentStatus.NOT_FOUND;
                 }
-                if (moveOp.getToRegister() != constRegister.get()) {
+                if (!constRegisters.contains(moveOp.getToRegister())) {
                     return ConstantParentStatus.NOT_FOUND;
                 }
+            } else {
+                // move-result(-object) without invoke-(*)
+                return ConstantParentStatus.UNKNOWN;
             }
             if (parameterRegisters.length == 1) {
                 // The constant result comes from constant parameter
-                constRegister.set(parameterRegisters[0]);
+                // No need to track original register anymore
+                constRegisters.remove(((MoveOp) currentOp).getToRegister());
+                constRegisters.add(parameterRegisters[0]);
             }
-            node.set(parentNode);
-            return ConstantParentStatus.SEEK_BACK;
-        } else if (parentOp instanceof GotoOp) {
-            // Goto has no side effect, all resgisters keeps the same
             node.set(parentNode);
             return ConstantParentStatus.SEEK_BACK;
         }
